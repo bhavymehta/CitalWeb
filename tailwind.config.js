e368361867a2f3e8fc2aaf2c86638277012fc340
Bhavy <bhavymehta10@gmail.com>
2023-11-25 17:01:05 +0530

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./src/web-pg/index.html", "./src/pr-pg/index.html", "./src/cart/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
